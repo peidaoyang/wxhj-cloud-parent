1e1c6ec4ca94edfba0f2d89631cc0bcc88113c2f
wxpjf2005 <592780385@qq.com>
2020-05-08 17:04:58 +0800

    解决Optional.of空指针问题

--- wxhj-cloud-subassembly/wxhj-cloud-core/src/main/java/com/wxhj/cloud/core/lock/DistributedLock.java
+++ wxhj-cloud-subassembly/wxhj-cloud-core/src/main/java/com/wxhj/cloud/core/lock/DistributedLock.java
@@ -1,22 +1,14 @@
 package com.wxhj.cloud.core.lock;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.cache.PathChildrenCache;
-import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
-import java.util.stream.Collectors;
 
 /**
  * @author wxpjf
@@ -31,13 +23,13 @@ import java.util.stream.Collectors;
 public class DistributedLock implements Lock {
     private static final String ROOT_LOCK = "/locks";
 
-    private static final String LOCK_SPLIT_STR = "_lock_";
+    // private static final String LOCK_SPLIT_STR = "_lock_";
     // 等待的前一个锁
-    private String WAIT_LOCK;
+    //   private String WAIT_LOCK;
     // 当前锁
-    private String CURRENT_LOCK;
+    //   private String CURRENT_LOCK;
 
-    private CountDownLatch countDownLatch;
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
     //
     private CuratorFramework curatorFramework;
     // 竞争的资源
@@ -46,8 +38,9 @@ public class DistributedLock implements Lock {
 
     private String nodePath() {
 
-        return ROOT_LOCK.concat("/").concat(lockName).concat(LOCK_SPLIT_STR);
+        return ROOT_LOCK.concat("/").concat(lockName);
     }
+
 
     public DistributedLock(CuratorFramework curatorFramework, String lockName) throws Exception {
         this.curatorFramework = curatorFramework;
@@ -61,101 +54,41 @@ public class DistributedLock implements Lock {
         }
     }
 
-
+    //争夺锁
     private void createNode() throws Exception {
-        CURRENT_LOCK = curatorFramework.create().creatingParentsIfNeeded()
-                .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+        curatorFramework.create().creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
                 .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
                 .forPath(this.nodePath());
     }
 
-    private boolean waitForLock(String prev) throws Exception {
-
-        curatorFramework.getData().usingWatcher(new Watcher() {
-            @Override
-            public void process(WatchedEvent event) {
-
-            }
-        });
-        //
-        if (curatorFramework.checkExists().forPath(ROOT_LOCK + "/" + prev) != null) {
-            System.out.println(Thread.currentThread().getName() + "等待锁 " + ROOT_LOCK + "/" + prev);
-            //  this.countDownLatch = new CountDownLatch(1);
-            // 计数等待，若等到前一个节点消失，则precess中进行countDown，停止等待，获取锁
-            //   this.countDownLatch.await(waitTime, timeUnit);
-            //  this.countDownLatch = null;
-        }
-        return true;
-    }
-
-    @SneakyThrows
+    //private void addWatcher()
     @Override
     public void lock() {
-        createNode();
-        if (this.tryLock()) {
-            System.out.println(Thread.currentThread().getName() + " " + lockName + "获得了锁");
-            return;
-        } else {
-            // 等待锁
-            waitForLock(WAIT_LOCK);
-        }
+
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
-        this.lock();
+
+
+        while (true) {
+            try {
+                createNode();
+                break;
+            } catch (Exception ex) {
+
+            }
+        }
     }
 
     @Override
     public boolean tryLock() {
-        try {
-            // 取所有子节点
-            List<String> subNodes =
-                    curatorFramework.
-                            getChildren().forPath(ROOT_LOCK); // 避免羊群效应
-            // 取出所有lockName的锁
-            List<String> lockObjects = subNodes.stream().filter(q ->
-                    q.split(LOCK_SPLIT_STR)[0].equals(lockName)
-            ).sorted().collect(Collectors.toList());
-
-            System.out.println("===" + lockObjects);
-            System.out.println(Thread.currentThread().getName() + " 的锁是 " + CURRENT_LOCK);
-            // 若当前节点为最小节点，则获取锁成功
-            if (CURRENT_LOCK.equals(ROOT_LOCK + "/" + lockObjects.get(0))) {
-                return true;
-            }
-            // 若不是最小节点，则找到自己的前一个节点
-            String prevNode = CURRENT_LOCK.substring(CURRENT_LOCK.lastIndexOf("/") + 1);
-            WAIT_LOCK = lockObjects.get(Collections.binarySearch(lockObjects, prevNode) - 1);
-            //添加节点监听
-            PathChildrenCache cache = new PathChildrenCache(curatorFramework, WAIT_LOCK, false);
-            cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
-            cache.getListenable().addListener((client, event) -> {
-                if (event.getType().equals(PathChildrenCacheEvent.Type.CHILD_REMOVED)) {
-                    String oldPath = event.getData().getPath();
-                    log.info("success to release lock for path:{}", oldPath);
-                    //if (oldPath.contains(path)) {
-                        //释放计数器，让当前的请求获取锁
-                        countDownLatch.countDown();
-                   // }
-                }
-            });
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
         return false;
     }
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        try {
-            if (this.tryLock()) {
-                return true;
-            }
-            return waitForLock(WAIT_LOCK);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return false;
     }
 
@@ -163,9 +96,8 @@ public class DistributedLock implements Lock {
     @Override
     public void unlock() {
         try {
-            System.out.println("释放锁 " + CURRENT_LOCK);
-            curatorFramework.delete().forPath(CURRENT_LOCK);
-            CURRENT_LOCK = null;
+            // System.out.println("释放锁 " + CURRENT_LOCK);
+            curatorFramework.delete().forPath(this.nodePath());
         } catch (Exception e) {
             log.error(e.getMessage());
         }
